6cc3261ba0dba5b20b8dbd16412918b3e388f9a9
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 15:51:29 +0100

    Fixa flexbox på productcard

--- Frontend_v2/src/components/OrderPage/ProductCard.tsx
+++ Frontend_v2/src/components/OrderPage/ProductCard.tsx
@@ -23,8 +23,8 @@ export default function ProductCard({
 }: ProductCardProps) { 
 
     return (
-        <div className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center">
-            <div className="w-32 h-32 mb-4">
+        <div className="bg-white shadow-md rounded-lg p-4 w-full max-w-md">
+            <div className="flex justify between items-center w-full mb-4">
                 <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
                 <button className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
                     Manage
@@ -33,9 +33,9 @@ export default function ProductCard({
             <div className="grid grid-cols-2 gap-4">
                 <div className="flex flex-col">
                     <span className="text-sm text-gray-500">baseStock</span>
-                    <span className="text-md font-medium text-gray-800">{baseStock}</span>
+                    <span className="font-semibold">{baseStock}</span>
                 </div>
+            </div>
         </div>
-    </div>
     );
 }
