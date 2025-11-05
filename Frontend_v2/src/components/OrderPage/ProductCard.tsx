857d53a527b3df976000eb66b160515e85f671a4
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 16:10:14 +0100

    enda en fix for flex

--- Frontend_v2/src/components/OrderPage/ProductCard.tsx
+++ Frontend_v2/src/components/OrderPage/ProductCard.tsx
@@ -24,7 +24,7 @@ export default function ProductCard({
 
     return (
         <div className="bg-white shadow-md rounded-lg p-4 w-full max-w-md mb-6">
-            <div className="flex justify between items-center mb-4">
+            <div className="flex justify-between items-center mb-4">
                 <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
                 <button className="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
                     Manage
