fafe42e6ce78e345c4a12dc00bb7495628811082
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 14:28:55 +0100

    Order Page content #1

--- Frontend_v2/src/pages/OrderPage.tsx
+++ Frontend_v2/src/pages/OrderPage.tsx
@@ -2,3 +2,13 @@
 import React, { useState } from "react";
 import type { Result } from "../types/result";
 import { navigate } from "rwsdk/client";
+
+const OrderPage = () => {
+    return (
+        <div>
+            <h1>Order Page</h1>
+            <p></p>
+        </div>
+    );
+}
+export default OrderPage;
