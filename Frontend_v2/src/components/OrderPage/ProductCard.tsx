c58bba81a1d8e7bc686448b97a653d367ca0976d
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 15:22:21 +0100

    Flere verdier i Props

--- Frontend_v2/src/components/OrderPage/ProductCard.tsx
+++ Frontend_v2/src/components/OrderPage/ProductCard.tsx
@@ -4,6 +4,11 @@ interface ProductCardProps {
     title: string;
     price: number;
     image: string;
+    baseStock?: number;
+    minimumStock?: number;
+    dailySales?: number;
+    supplyTimeDays?: number;
+    Status: "good | warning | critical";
 }
 
 export default function ProductCard() {
