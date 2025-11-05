d5348a890a15c5958dc08cb7452863727b29c7b6
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 15:19:58 +0100

    Start på Product card

---
+++ Frontend_v2/src/components/OrderPage/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface ProductCardProps {
+    title: string;
+    price: number;
+    image: string;
+}
+
+export default function ProductCard() {
+    return (
+        <div>ProductCard</div>
+    );
+}
