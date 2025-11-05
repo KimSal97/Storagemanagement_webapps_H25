38b338dbf3c02b80bd4521387fd80f352c958e56
notveryhelpfuldude <caracass@hotmail.no>
2025-11-05 16:51:51 +0100

    la til grid i orderpage

--- Frontend_v2/src/pages/OrderPage.tsx
+++ Frontend_v2/src/pages/OrderPage.tsx
@@ -17,6 +17,7 @@ const OrderPage = () => {
           <h1 className="text-2xl font-semibold text-gray-800 mb-4">
             Bestillinger
           </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <ProductCard 
                 title="Product Name"
                 price={29.99}
@@ -35,6 +36,7 @@ const OrderPage = () => {
                 supplyTimeDays={14}
                 Status="warning"
             />
+            </div>
         </main>
       </div>
     </div>
