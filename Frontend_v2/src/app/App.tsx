9a4a1e2b46ed4171a4bdb932d7b998cea2ddee3f
notveryhelpfuldude <caracass@hotmail.no>
2025-11-30 15:20:10 +0100

    Fikset filnavn

--- Frontend_v2/src/app/App.tsx
+++ Frontend_v2/src/app/App.tsx
@@ -5,7 +5,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import RegisterPage from "@/pages/RegisterpageTEMP";
+import RegisterPage from "@/pages/RegisterPage";
 import LoginPage from "@/pages/LoginPage";
 import OrderPage from "@/pages/OrderPage";
 
