5c85a4ad15a1061706ee40e7a30f062ea812f899
notveryhelpfuldude <caracass@hotmail.no>
2025-12-01 17:02:14 +0100

    supplierService Gooddata test

--- Frontend_v2/src/features/suppliers/suppliersService.test.ts
+++ Frontend_v2/src/features/suppliers/suppliersService.test.ts
@@ -3,7 +3,7 @@ import { describe, vi, type Mocked, beforeEach } from "vitest";
 
 //Fake the repository module
 vi.mock("./suppliersRepository", async () => ({
-  suppliersRepository: {
+    suppliersRepository: {
     findByEmail: vi.fn(),
     create: vi.fn(),
     findAll: vi.fn(),
@@ -19,7 +19,20 @@ import { suppliersRepository } from "./suppliersRepository";
 const mockRepo = suppliersRepository as Mocked<typeof suppliersRepository>;
 
 describe("suppliersService", () => {
-  beforeEach(() => {
+    beforeEach(() => {
     vi.clearAllMocks();
-  });
+    });
+    describe("createSupplier", () => {
+        const goodData = {
+            name: "Supplier John",
+            contact_person: "John Doe",
+            email: "johnA@mail.com",
+            phone: "12345678",
+            address: "123 Street",
+            status: "Aktiv" as const,
+        };
+
+
+    });
+
 });
