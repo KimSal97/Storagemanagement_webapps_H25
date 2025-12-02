5c3a5ad9893667adae4eb7d9967d373051a1c8dc
notveryhelpfuldude <caracass@hotmail.no>
2025-12-02 16:49:27 +0100

    Test db lager tabell manuelt

--- Frontend_v2/test_utils/createTestDb.ts
+++ Frontend_v2/test_utils/createTestDb.ts
@@ -1,10 +1,22 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import * as schema from "@/db/";
 import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+
 
 export function createTestDb() {
   const sqlite = new Database(":memory:");
-  const db = drizzle(sqlite, { schema });
 
+  sqlite.exec(`
+    CREATE TABLE suppliers (
+      id TEXT PRIMARY KEY,
+      name TEXT NOT NULL,
+      contact_person TEXT NOT NULL,
+      email TEXT NOT NULL,
+      phone TEXT NOT NULL,
+      address TEXT,
+      status TEXT NOT NULL
+    );
+  `);
+
+  const db = drizzle(sqlite);
   return db;
 }
